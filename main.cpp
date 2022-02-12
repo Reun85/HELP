3c36b98d49adcddeea7aadcc10a8eb2410597285
Reun85 <59803338+Reun85@users.noreply.github.com>
2022-02-12 15:57:53 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+
+
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+#include <tuple>
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <chrono>
+#include <fstream>
+#include <bitset>
+#include <filesystem>
+
+
+#include <thread>
+#include <mutex>
